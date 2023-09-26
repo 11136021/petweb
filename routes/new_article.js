5188f25b6f696d522426bb45ff344394a7228733
W-christine <11136035@ntub.edu.tw>
2023-09-26 10:32:28 +0800

    網頁連結修正、新增貼文(跳轉選擇)

--- routes/new_article.js
+++ routes/new_article.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const{ArticleModel} = require('../model.js');
 var router = express.Router();
 
 /* GET home page. */
@@ -7,8 +8,38 @@ router.get('/', function(req, res, next) {
     var email = req.session.email;
     res.render('new_article');
   }else{
-    res.redirect('/user_login')
+    res.redirect('/user_login') //session
   }
 });
+
+router.post('/',async function(req, res, next) {
+
+  if(req.session.email){
+    var email = req.session.email;
+    var title = req.body.title;
+    var category = req.body.category;
+    var content = req.body.content;
+
+        
+    // 建立一個新資料物件
+    const newData = new ArticleModel({
+      email: email,
+      title: title,
+      category: category,
+      content: content
+    });
+
+    newData.save()
+    res.redirect("/main");
+
+
+
+  }else{
+    res.redirect('/user_login') //session
+  }
+
+  
+});
+
 
 module.exports = router;
