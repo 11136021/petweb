a8cdaafeae73e26f6bc645aa6408965f5c75a400
W-christine <11136035@ntub.edu.tw>
2023-09-29 14:15:50 +0800

    走失、旅遊版js更新完成

--- routes/lost_lookfor.js
+++ routes/lost_lookfor.js
@@ -1,13 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const { ArticleModel } = require('../model.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  if(req.session.email){
+router.get('/', function (req, res, next) {
+  if (req.session.email) {
     var email = req.session.email;
-    res.render('lost_lookfor');
+
+    ArticleModel.find( //從資料庫中抓取符合條件的資料
+      { category: 'recipe' }
+    ).exec()
+      .then(function (model) {
+        if (model.length === 0) {
+          console.log('lost_lookfor, model length == 0');
+          res.redirect("/user_addFail");
+        } else { //有抓到資料，表示符合條件，即登入成功
+          req.session.email = email //登入成功之後用email來存
+          console.log('lost_lookfor, model length == ' + model.length);
+          res.render('lost_lookfor', { model: model });
+        }
+      })
+      .catch(function (err) { //觸發例外，先不管，直接跳登入失敗
+        console.log(err);
+        res.redirect("/user_addFail");
+      })
   }else{
-    res.redirect('/user_login')
-  }
-});
+  res.redirect("/user_login");
+}
+}
+);
+
 module.exports = router;
