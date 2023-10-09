4bd645d44e814aeb94810fdaca808b62901d28eb
11136021 <11136021@ntub.edu.tw>
2023-10-09 19:04:38 +0800

    生食版新增貼文完成

--- routes/cook_kan_rare.js
+++ routes/cook_kan_rare.js
@@ -30,4 +30,35 @@ router.get('/', function (req, res, next) {
 }
 );
 
+
+router.post('/',async function(req, res, next) {
+
+  if(req.session.email){
+    var email = req.session.email;
+    var title = req.body.title;
+    var category = req.body.category;
+    var content = req.body.content;
+        
+    // 建立一個新資料物件
+    const newData = new ArticleModel({
+      email: email,
+      title: title,
+      category: category,
+      subcate:'rare',
+      content: content
+    });
+
+    newData.save()
+    res.redirect('/cook_kan_rare') 
+    }else{
+      res.redirect('/user_login') //session
+    }
+
+
+    //送出貼文之後，根據發布貼文所選擇的類別，跳轉到該類別的瀏覽畫面
+    //用name去改
+  
+});
+
+
 module.exports = router;
