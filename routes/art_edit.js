0a5cf36f7b4f809701845e569a11924c8d8f778a
11136021 <11136021@ntub.edu.tw>
2023-10-20 13:23:11 +0800

    編輯貼文抓去資料

--- routes/art_edit.js
+++ routes/art_edit.js
@@ -3,8 +3,26 @@ var router = express.Router();
 const { ArticleModel } = require('../model.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('art_edit');
-});
+router.get('/', function (req, res, next) {
+  if (req.session.email) {
+    var email = req.session.email;
+
+    ArticleModel.find( //從資料庫中抓取符合條件的資料
+      { email: email }
+    ).exec()
+      .then(function (model) {
+        
+          console.log('art_edit, model length == ' + model.length);
+          res.render('art_edit', { model: model });
+        
+      })
+      .catch(function (err) { //觸發例外，先不管，直接跳登入失敗
+        console.log(err);
+        res.redirect("/user_addFail");
+      })
+  }else{
+  res.redirect("/user_login");
+}});
+
 
 module.exports = router;
