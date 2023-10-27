9d6b6f791586b9d10a837260f216b5cb0e647b92
W-christine <11136035@ntub.edu.tw>
2023-10-27 14:51:19 +0800

    醫療版

--- routes/medical_kan_med_organ.js
+++ routes/medical_kan_med_organ.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const { ArticleModel } = require('../model.js');
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -31,14 +32,34 @@ router.post('/',async function(req, res, next) {
     var title = req.body.title;
     var category = req.body.category;
     var content = req.body.content;
-        
+ 
+    //新增圖片
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return res.status(400).send('No files were uploaded.');
+    }
+  
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    sampleFile = req.files.photo;
+    uploadPath = path.join(__dirname, '/..', '/photo/') + sampleFile.name;
+    console.log(`fileuploadpath: ${uploadPath}`);
+    // Use the mv() method to place the file somewhere on your server
+    sampleFile.mv(uploadPath, function(err) {
+      if (err)
+        return res.status(500).send(err);
+
+      console.log("file uploaded");
+      //res.send('File uploaded!');
+    });
+    
+
     // 建立一個新資料物件
     const newData = new ArticleModel({
       email: email,
       title: title,
       category: category,
       subcate:'med_organ',
-      content: content
+      content: content,
+      photo:sampleFile.name
     });
 
     newData.save()
