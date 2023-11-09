b60cfd3c853bd8d062179b2ccdc983aa2b444c9c
11136021 <11136021@ntub.edu.tw>
2023-11-09 13:05:10 +0800

    顯示個人檔案畫面及資料

--- routes/profile.js
+++ routes/profile.js
@@ -1,14 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const { UserModel } = require('../model.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  if(req.session.email){
+router.get('/', function (req, res, next) {
+  if (req.session.email) {
     var email = req.session.email;
-    res.render('profile');
+
+    UserModel.find( //從資料庫中抓取符合條件的資料
+      { email: email }
+    ).exec()
+      .then(function (model) {
+        
+          console.log('cook_kan_cooked, model length == ' + model.length);
+          res.render('profile', { model: model });
+        
+      })
+      .catch(function (err) { //觸發例外，先不管，直接跳登入失敗
+        console.log(err);
+        res.redirect("/user_addFail");
+      })
   }else{
-    res.redirect('/user_login')
-  }
-});
+  res.redirect("/user_login");
+}});
 
 module.exports = router;
