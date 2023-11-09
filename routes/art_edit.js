0647261d20ad5379180efdad6b577df93e8c83cf
W-christine <11136035@ntub.edu.tw>
2023-11-09 09:16:09 +0800

    修改貼文

--- routes/art_edit.js
+++ routes/art_edit.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const { ArticleModel } = require('../model.js');
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -51,11 +52,32 @@ router.post('/edit/:id', function (req, res, next) {
   if (req.session.email) {
     
     var id = req.params.id;
-    var title = req.body.title;
+    var title = req.body.edit_title;
     var content = req.body.edit_content;
+    var sampleFile = '';
+    
+    try{
+      if (req.files || Object.keys(req.files).length !== 0) {
+        // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+        sampleFile = req.files.photo;
+        uploadPath = path.join(__dirname, '/..', '/photo/') + sampleFile.name;
+        console.log(`fileuploadpath: ${uploadPath}`);
+        // Use the mv() method to place the file somewhere on your server
+        sampleFile.mv(uploadPath, function(err) {
+          if (err)
+            return res.status(500).send(err);
+          else
+            console.log("file uploaded");
+        });
+      }
+    }catch{}
 
     var queryValue = {_id:id};
-    var editValues = {title: title, content: content };
+    if (sampleFile == '')
+      var editValues = {title: title, content: content };
+    else
+      var editValues = {title: title, content: content,photo: sampleFile.name };
+    
     // {查詢,修改成}
     ArticleModel.updateOne( //從資料庫中抓取符合條件的資料
       queryValue, editValues
