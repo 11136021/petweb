a9f10471fd09d26c75a50d1383f88ce6818f5f6d
11136023 <11136023@ntub.edu.tw>
2023-12-13 15:24:26 +0800

    1213

--- routes/profile.js
+++ routes/profile.js
@@ -8,86 +8,67 @@ router.get('/', function (req, res, next) {
   if (req.session.email) {
     var email = req.session.email;
     
-    UserModel.find( //從資料庫中抓取符合條件的資料
-      { email: email }
-    ).exec()
+    UserModel.find({ email: email }).exec()
       .then(function (model) {
-          console.log('profile, model length == ' + model.length);
-          console.log('model: ' + model[0]);
-          res.render('profile', { model: model });
-        
+        console.log('profile, model length == ' + model.length);
+        console.log('model: ' + model[0]);
+        res.render('profile', { model: model });
       })
-      .catch(function (err) { //觸發例外，先不管，直接跳登入失敗
+      .catch(function (err) {
         console.log(err);
         res.redirect("/user_addFail");
       })
-  }else{
-  res.redirect("/user_login");
-}});
+  } else {
+    res.redirect("/user_login");
+  }
+});
 
-router.post('/',async function(req, res, next) {
-
-  if(req.session.email){
+router.post('/', async function(req, res, next) {
+  if (req.session.email) {
     var email = req.session.email;
-    var photo = req.body.photo;
     var nickname = req.body.nickname;
     var introduction = req.body.introduction;
-    var petname = req.body.pets;
+    var petname = req.body.petname;
     var birthday = req.body.birthday;
         
-  //新增圖片
+    // 新增圖片
     if (!req.files || Object.keys(req.files).length === 0) {
       return res.status(400).send('No files were uploaded.');
     }
   
-    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-    sampleFile = req.files.photo;
-    uploadPath = path.join(__dirname, '/..', '/photo/') + sampleFile.name;
+    const sampleFile = req.files.photo;
+    const uploadPath = path.join(__dirname, '/..', '/photo/') + sampleFile.name;
     console.log(`fileuploadpath: ${uploadPath}`);
-    // Use the mv() method to place the file somewhere on your server
-    sampleFile.mv(uploadPath, function(err) {
-      if (err)
+    
+    // 移動上傳的檔案到伺服器端
+    sampleFile.mv(uploadPath, async function(err) {
+      if (err) {
         return res.status(500).send(err);
+      }
 
-      console.log("file uploaded");
-      //res.send('File uploaded!');
-    });
-    
-    UserModel.find( //從資料庫中抓取符合條件的資料
-    { email: email }
-  ).exec()
-    .then(function (model) {
-        console.log('post profile, model length == ' + model.length);
-        console.log('post model: ' + model[0]);
-        model[0].birthday = birthday;
-        model[0].nickname = nickname;
-        model[0].save();
-        res.render('profile', { model: model });
-      
-    })
-    .catch(function (err) { //觸發例外，先不管，直接跳登入失敗
-      console.log(err);
-      res.redirect("/user_addFail");
-    })
-    
-    // 建立一個新資料物件
-    /*
-    const newData = new UserModel({
-      email: email,
-      birthday: birthday,   //生日
-      petname: petname,//寵物名稱
-      introduction: introduction, //自介
-      photo: photo ,//頭貼
-      nickname: nickname,
-    });
+      try {
+        const userModel = await UserModel.findOne({ email });
 
-    newData.save()
-    res.redirect('/profile') 
-    */
-  }else{
-      res.redirect('/user_login') //session
+        if (userModel) {
+          userModel.birthday = birthday;
+          userModel.nickname = nickname;
+          userModel.petname = petname;
+          userModel.introduction = introduction;
+          userModel.photo = sampleFile.name;
+          
+          await userModel.save();
+          res.redirect('/profile');
+        } else {
+          throw new Error('User not found');
+        }
+      } catch (err) {
+        console.log(err);
+        res.redirect("/user_addFail");
+      }
+    });
+  } else {
+    res.redirect('/user_login');
   }
-
-  
 });
+
 module.exports = router;
